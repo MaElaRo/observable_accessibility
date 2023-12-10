18d321ed8f5eb519825d18d092f4981f02b84485
MaElaRo <manuelasakurarommel@yahoo.de>
2023-12-10 23:16:36 +0100

    onboarding page view

--- lib/features/onboarding/pages/onboarding_page.dart
+++ lib/features/onboarding/pages/onboarding_page.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:observable_accessibility/features/onboarding/models/onboarding_step.dart';
 import 'package:observable_accessibility/features/onboarding/widgets/onbording_step_page_view.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +30,33 @@ class OnboardingPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tutorial'),
       ),
-      body: PageView.builder(
-        itemCount: OnboardingStep.values.length,
-        itemBuilder: (context, index) => OnboardingStepPageView(
-          onboardingStep: OnboardingStep.values[index],
+      body: MergeSemantics(
+        child: Semantics(
+          liveRegion: true,
+          onScrollDown: () => _setPageIndex(_pageController.page!.toInt() - 1),
+          onScrollUp: () => _setPageIndex(_pageController.page!.toInt() + 1),
+          onIncrease: () => _setPageIndex(_pageController.page!.toInt() + 1),
+          onDecrease: () => _setPageIndex(_pageController.page!.toInt() - 1),
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: OnboardingStep.values.length,
+            itemBuilder: (context, index) => OnboardingStepPageView(
+              onboardingStep: OnboardingStep.values[index],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  void _setPageIndex(int index) {
+    print('fishes');
+    if (index >= 0 && index < OnboardingStep.values.length) {
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 }
