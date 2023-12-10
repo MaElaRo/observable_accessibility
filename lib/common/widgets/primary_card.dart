7dc95bfb228b4317258f59b1cb9ef99fb64f8b4a
MaElaRo <manuelasakurarommel@yahoo.de>
2023-12-10 12:06:55 +0100

    deleted ontap

--- lib/common/widgets/primary_card.dart
+++ lib/common/widgets/primary_card.dart
@@ -3,41 +3,36 @@ import 'package:flutter/material.dart';
 class PrimaryCard extends StatelessWidget {
   const PrimaryCard({
     required this.child,
-    this.onTap,
     this.padding,
     super.key,
   });
 
   final Widget child;
-  final VoidCallback? onTap;
   final EdgeInsets? padding;
 
   @override
   Widget build(BuildContext context) {
     final borderRadius = BorderRadius.circular(10);
 
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        border: Border.all(color: Theme.of(context).colorScheme.outlineVariant),
-        borderRadius: borderRadius,
-        boxShadow: [
-          BoxShadow(
-            color: Theme.of(context).colorScheme.shadow,
-            blurRadius: 3,
-          ),
-        ],
-      ),
-      child: Material(
-        type: MaterialType.transparency,
-        child: InkWell(
+    return Semantics(
+      explicitChildNodes: true,
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          border:
+              Border.all(color: Theme.of(context).colorScheme.outlineVariant),
           borderRadius: borderRadius,
-          onTap: onTap,
-          child: Padding(
-            padding: padding ?? const EdgeInsets.all(16),
-            child: child,
-          ),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.shadow,
+              blurRadius: 3,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: padding ?? const EdgeInsets.all(16),
+          child: child,
         ),
       ),
     );
