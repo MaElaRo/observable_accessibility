43a0dee4754f884359421aedeaa3dbc71a614979
MaElaRo <manuelasakurarommel@yahoo.de>
2023-12-14 14:36:25 +0100

    added indicator

--- lib/features/onboarding/pages/onboarding_page.dart
+++ lib/features/onboarding/pages/onboarding_page.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:observable_accessibility/common/widgets/page_indicator.dart';
 import 'package:observable_accessibility/features/onboarding/models/onboarding_step.dart';
 import 'package:observable_accessibility/features/onboarding/widgets/onbording_step_page_view.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +31,26 @@ class OnboardingPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tutorial'),
       ),
-      body: PageView.builder(
-        itemCount: OnboardingStep.values.length,
-        itemBuilder: (context, index) => OnboardingStepPageView(
-          onboardingStep: OnboardingStep.values[index],
-        ),
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: OnboardingStep.values.length,
+            itemBuilder: (context, index) => OnboardingStepPageView(
+              onboardingStep: OnboardingStep.values[index],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: 128,
+              child: PageIndicator(
+                pagesCount: OnboardingStep.values.length,
+                controller: _pageController,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
